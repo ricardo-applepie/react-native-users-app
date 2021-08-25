8ed1b707cdb04efb9c03608d63e03f823f8a9db2
ricardo-applepie <ricardo@applepie.berlin>
2021-08-26 00:27:01 +0200

    connected backend server to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+const express = require('express');
+var bodyParser = require('body-parser')
 
-app.get('/users', function (req, res) {
-    res.send('Hello World')
+
+const db = require('./db');
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/users', async function  (req, res) {
+try{
+    const results = await db.query('SELECT * FROM restaurants');
+    console.log(results)
+    res.send("hi")
+}catch(e){
+console.log(e)
+}
 })
 
-app.listen(3000)
+app.listen(3000,()=>console.log('server running on 3000'))
