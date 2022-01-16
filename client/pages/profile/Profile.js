5ebafad2975f4f14e0c130d78723b8e6db07be97
Fullstack web developer <84002031+ricardo-applepie@users.noreply.github.com>
2022-01-16 02:19:17 +0100

    Update Profile.js

--- client/pages/profile/Profile.js
+++ client/pages/profile/Profile.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-
 import {View, Text, Button, StyleSheet} from 'react-native';
 import UserInformation from '../../components/userInformation/UserInformation';
 import ProfileImage from '../../components/profileImage/ProfileImage';
